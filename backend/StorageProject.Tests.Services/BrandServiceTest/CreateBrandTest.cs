b9d3ffdaa13a08b09bfa67f1843a4cef5091cf33
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2025-08-21 19:03:50 -0300

    test: create success product

--- backend/StorageProject.Tests.Services/BrandServiceTest/CreateBrandTest.cs
+++ backend/StorageProject.Tests.Services/BrandServiceTest/CreateBrandTest.cs
@@ -1,5 +1,7 @@
-﻿using Moq;
+﻿using Ardalis.Result;
+using Moq;
 using StorageProject.Application.DTOs.Brand;
+using StorageProject.Application.Mappers;
 using StorageProject.Domain.Entity;
 
 namespace StorageProject.Tests.Services.BrandServiceTest
@@ -20,11 +22,17 @@ namespace StorageProject.Tests.Services.BrandServiceTest
         {
             //Arrange
             var dto = new CreateBrandDTO{ Name = "Teste" };
-            var brand = new BrandDTO{ Id=Guid.NewGuid(),Name = dto.Name };
+            var brandDto = new BrandDTO{ Id=Guid.NewGuid(),Name = dto.Name };
+            var newBrand = new Brand { Id = brandDto.Id, Name = brandDto.Name };
 
             //to do: fix test cause the return is null and crash the test in DTO mapper
-            _fixture.UnitOfWorkMock.Setup(c => c.BrandRepository.GetByNameAsync(dto.Name, cancellationToken)).ReturnsAsync(new Brand { Id =brand.Id, Name = brand.Name });
-            _fixture.UnitOfWorkMock.Setup(c => c.BrandRepository.Create(It.IsAny<Brand>(), cancellationToken));
+            _fixture.UnitOfWorkMock.Setup(c => c.
+                BrandRepository.GetByNameAsync(dto.Name, cancellationToken))
+                               .ReturnsAsync(value: null);
+
+            _fixture.UnitOfWorkMock.Setup(c => c
+            .BrandRepository.Create(It.IsAny<Brand>(), cancellationToken))
+                            .ReturnsAsync(newBrand);
 
             _fixture.UnitOfWorkMock.Setup(c => c.CommitAsync());
 
@@ -39,10 +47,15 @@ namespace StorageProject.Tests.Services.BrandServiceTest
         {
             //Arrange
             var dto = new CreateBrandDTO { Name = "10" };
-            var brand = new BrandDTO { Name = dto.Name };
+            var brandDto = new BrandDTO { Name = dto.Name };
+            var newBrand = new Brand { Id = brandDto.Id, Name = brandDto.Name };
 
-            _fixture.UnitOfWorkMock.Setup(c => c.BrandRepository.GetByNameAsync(dto.Name, cancellationToken)).ReturnsAsync(value: null);
-            _fixture.UnitOfWorkMock.Setup(c => c.BrandRepository.Create(It.IsAny<Brand>(), cancellationToken));
+            _fixture.UnitOfWorkMock.Setup(c => c.BrandRepository.GetByNameAsync(dto.Name, cancellationToken))
+                                    .ReturnsAsync(value: null);
+
+            _fixture.UnitOfWorkMock.Setup(c => c.BrandRepository.Create(It.IsAny<Brand>(), cancellationToken))
+                .ReturnsAsync(newBrand);
+
             _fixture.UnitOfWorkMock.Setup(c => c.CommitAsync());
 
             var result = await _fixture.Service.CreateAsync(dto);
