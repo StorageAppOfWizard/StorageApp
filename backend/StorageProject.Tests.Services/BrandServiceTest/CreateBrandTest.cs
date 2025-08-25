bbc2274e60bd1946601bba8247a47c7a03f692ac
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2025-08-25 09:32:42 -0300

    test: createBrand

--- backend/StorageProject.Tests.Services/BrandServiceTest/CreateBrandTest.cs
+++ backend/StorageProject.Tests.Services/BrandServiceTest/CreateBrandTest.cs
@@ -2,6 +2,7 @@
 using Moq;
 using StorageProject.Application.DTOs.Brand;
 using StorageProject.Application.Mappers;
+using StorageProject.Application.Validators;
 using StorageProject.Domain.Entity;
 
 namespace StorageProject.Tests.Services.BrandServiceTest
@@ -43,25 +44,17 @@ namespace StorageProject.Tests.Services.BrandServiceTest
         }
         
         [Fact]
-        public async Task CreateBrand_WhenFieldsAreNotCorrect_ReturnBadRequestresult()
+        public async Task CreateBrand_WhenNameFieldIncorrect_ReturnBadRequestresult()
         {
             //Arrange
-            var dto = new CreateBrandDTO { Name = "10" };
-            var brandDto = new BrandDTO { Name = dto.Name };
-            var newBrand = new Brand { Id = brandDto.Id, Name = brandDto.Name };
+            var dto = new CreateBrandDTO { Name = "" };
 
-            _fixture.UnitOfWorkMock.Setup(c => c.BrandRepository.GetByNameAsync(dto.Name, cancellationToken))
-                                    .ReturnsAsync(value: null);
-
-            _fixture.UnitOfWorkMock.Setup(c => c.BrandRepository.Create(It.IsAny<Brand>(), cancellationToken))
-                .ReturnsAsync(newBrand);
-
-            _fixture.UnitOfWorkMock.Setup(c => c.CommitAsync());
-
-            var result = await _fixture.Service.CreateAsync(dto);
-            var objectResult = Assert.IsType<BrandDTO>(result.Value);
-
-            Assert.Equal(dto.Name, objectResult.Name);
+            //Act
+            var result = await _fixture.Service.CreateAsync(dto); 
+           
+            //Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal(result, Result.Invalid());
         }
 
     }
