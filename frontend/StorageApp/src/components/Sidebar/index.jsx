35f944cd01e2b017d56dd5964289b43c026dd45e
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2025-08-26 01:50:28 -0300

    Update index.jsx

--- frontend/StorageApp/src/components/Sidebar/index.jsx
+++ frontend/StorageApp/src/components/Sidebar/index.jsx
@@ -1,29 +1,32 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { ShoppingCart, Package, Users, DollarSign, Menu, Settings } from 'lucide-react'
-import "./sidebar.css"
+import { ShoppingCart, Package, Users, DollarSign, Menu, Settings } from 'lucide-react';
+import "./sidebar.css";
+import logo from '../../assets/Logo-teste.png';
 
 export default function Sidebar() {
     const [isOpen, setIsOpen] = useState(false);
 
     const menuItems = [
-        { name: "Pedidos", icon: ShoppingCart, path: "" },
-        { name: "Produtos", icon: Package, path: "" },
-        { name: "Clientes", icon: Users, path: "" },
-        { name: "Históricos", icon: DollarSign, path: "" },
-        { name: "Usuários", icon: Users, path: "" },
-        { name: "Configurações", icon: Settings, path: "" }
+        { name: "Pedidos", icon: ShoppingCart, path: "/pedidos" },
+        { name: "Produtos", icon: Package, path: "/" },
+        { name: "Clientes", icon: Users, path: "/clientes" },
+        { name: "Históricos", icon: DollarSign, path: "/historicos" },
+        { name: "Usuários", icon: Users, path: "/usuarios" },
+        { name: "Configurações", icon: Settings, path: "/configuracoes" }
     ];
 
     return (
         <>
-            {/* Botão do menu no topo (mobile) */}
             <button className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
                 <Menu size={24} />
             </button>
 
-             {/* Sidebar */}
             <nav className={`sidebar ${isOpen ? "open" : ""}`}>
+                <div className="logo">
+                    <img src={logo} alt="Logo da Marca" />
+                </div>
+
                 <div className="menu">
                     {menuItems.map((item) => {
                         const Icon = item.icon;
@@ -34,20 +37,17 @@ export default function Sidebar() {
                                 className={({ isActive }) =>
                                     `menu-item ${isActive ? "active" : ""}`
                                 }
-                                onClick={() => setIsOpen(false)} // Fecha o mobile
+                                onClick={() => setIsOpen(false)}
                             >
                                 <Icon className="icon" />
                                 <span>{item.name}</span>
                             </NavLink>
-                        )
-                    })
-
-                    }
+                        );
+                    })}
                 </div>
             </nav>
 
-             {/* Fundo escuro no mobile*/}  
             {isOpen && <div className="overlay" onClick={() => setIsOpen(false)}></div>}
         </>
-    )
+    );
 }
