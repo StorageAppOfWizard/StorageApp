fbe1daa3e3de4af9a5ae9b6e0e2430b0dee29035
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2025-10-14 18:59:56 -0300

    cleaner: clean up program

---
+++ backend/src/StorageProject.Api/Configurations/ApiConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using StorageProject.Application.Validators;
+using System.Text.Json.Serialization;
+
+namespace StorageProject.Api.Configurations
+{
+    public static class ApiConfiguration
+    {
+        public static void AddApiConfiguration(this IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+            services.AddValidatorsFromAssemblyContaining<ProductValidator>();
+
+            services.AddEndpointsApiExplorer();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigins", policy =>
+                {
+                    policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+        }
+    }
+}
---
+++ backend/src/StorageProject.Api/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace StorageProject.Api.Configurations
+{
+    public static class SwaggerConfiguration
+    {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.EnableAnnotations();
+            });
+
+        }
+
+    }
+}
