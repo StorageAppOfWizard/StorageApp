e9e505cdcb3d337afe00968fa40fc5c5c60099bc
gabri <gabri@PC-GABRIEL>
2026-02-13 00:44:04 -0300

    feat: open telemetry to new relic

--- backend/src/StorageProject.Api/Program.cs
+++ backend/src/StorageProject.Api/Program.cs
@@ -1,14 +1,20 @@
 using DotNetEnv;
+using OpenTelemetry.Logs;
 using StorageProject.Api.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Env.Load();
-
 builder.Services.AddApiConfiguration(builder.Configuration);
 builder.Services.AddSwaggerConfiguration();
 builder.Services.AddApplicationConfiguration();
 builder.Services.AddInfrastructureConfiguration(builder.Configuration);
+
+
+builder.Logging.AddOpenTelemetry(o =>
+{     o.IncludeFormattedMessage = true;
+    o.AddOtlpExporter();
+});
 
 var app = builder.Build();
 app.AddPipelineConfiguration();
