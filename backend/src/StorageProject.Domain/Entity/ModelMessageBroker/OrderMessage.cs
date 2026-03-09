3ebf6dac3837e40fc04800565ce7629e70b91bf7
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2026-03-09 19:46:54 -0300

    init config consumer

---
+++ backend/src/StorageProject.Domain/Entity/ModelMessageBroker/OrderMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace StorageProject.Domain.Entity.ModelMessageBroker
+{
+    public class OrderMessage
+    {
+        public Guid Id { get; set; }
+        public int Quantity { get; set; }
+    }
+}
